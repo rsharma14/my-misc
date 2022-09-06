0210c6e0aed655898807c97ee9f7e9d637115ba4
Rajendra <you@example.com>
2022-09-06 11:27:21 +0530

    ok

--- Shut_Off_PC.bat
+++ Shut_Off_PC.bat
@@ -1,6 +1,6 @@
 @echo off
-SET SDTIME=100
-SET /p "SDTIME=Enter time to shutdown:"
+SET SDTIME=1
+SET /p "SDTIME=Enter time to shutdown in sec:"
 echo "System will be shutdown after %SDTIME% sec."
 shutdown /a
 shutdown /s /t %SDTIME% /f
