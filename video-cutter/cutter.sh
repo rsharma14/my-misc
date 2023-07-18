c4674f9499288b1058c92bbb9aa59d1a816d3812
rajendra <rajendra@email.com>
2023-07-18 20:04:52 +0530

    cc

--- video-cutter/cutter.sh
+++ video-cutter/cutter.sh
@@ -3,10 +3,13 @@
 #echo 'C:\Users\ac49999\git\APPS\2.MPG\my-misc\video-cutter\a.mp4'
 DT=$(date '+%Y%m%d%H%M%S')
 echo "DT=$DT"
-mkdir $DT
+PAR_FOLDER="Downloads"
+mkdir ~/$PAR_FOLDER/$DT
 
 read -r -p "Enter clips times in the format [[1,2],[3,2]]: " array_str
+array_str=[["00:00:00","00:00:10"],["00:00:35","00:00:48"],["00:00:52","00:01:00"]]
 read -r -p "Enter video file location to be clipped: " ip_video
+ip_video="C:\Users\ac49999\git\APPS\2.MPG\my-misc\video-cutter\a.mp4"
 ip_video=$(echo $ip_video | sed 's|\\|/|g')
 
 echo "ip_video="$ip_video
@@ -27,10 +30,11 @@ for element in "${arrays[@]}"; do
         END="${array[1]//\"/}"
 
         echo "$START:$END:$idx"
-        echo "ffmpeg -ss $START -to $END -i $ip_video -c copy $DT/$idx$OUTPUT"
-        ffmpeg -ss $START -to $END -i "$ip_video" -c copy "$DT/$idx$OUTPUT"
+        echo "ffmpeg -ss $START -to $END -i $ip_video -c copy  ~/$PAR_FOLDER/$DT/$idx$OUTPUT"
+        ffmpeg -ss $START -to $END -i "$ip_video" -c copy ~/$PAR_FOLDER/$DT/$idx$OUTPUT
         ((idx++))
+		echo "----------$idx clip completed----------"
     fi
 done
 
-echo "<===Total $idx clips are saved in $DT folder===>"
+echo "<===Total $idx clips are saved in ~/$PAR_FOLDER/$DT folder===>"
